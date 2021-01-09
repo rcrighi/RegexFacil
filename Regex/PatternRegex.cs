86f799ff60d575b3cb1cc6c29c5045404585618d
Rodrigo Cesar Righi <rcrighi@gmail.com>
2021-01-09 08:42:26 -0300

    Ajuste Pattern Telefone

--- Regex/PatternRegex.cs
+++ Regex/PatternRegex.cs
@@ -10,7 +10,7 @@ namespace Regex
     {
         public static string CPF = @"\d{3}\.\d{3}\.\d{3}\-\d{2}";
         public static string CNPJ = @"[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}\-?[0-9]{2}";
-        public static string TELEFONE = @"^\(\d{3}\)-\d{4}-\d{4}$";
+        public static string TELEFONE = @"[(]?\d{2}[)]?\s*[1-9]?\d{4}-?\d{4}";
         public static string EMAIL = @"^([\w\-]+\.)*[\w\- ]+@([\w\- ]+\.)+([\w\-]{2,3})$";
         public static string URL = @"^((http)|(https)|(ftp)):\/\/([\- \w]+\.)+\w{2,3}(\/ [%\-\w]+(\.\w{2,})?)*$";
         public static string IP = @"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b";
